184ebee8e1fef7624df8fc72b98cf940765576df
JaesungMoon <jsdekiru@gmail.com>
2017-06-18 14:11:28 +0900

    WebRTCの呪文

--- WebRTCHandsOn/view/ChatViewController.swift
+++ WebRTCHandsOn/view/ChatViewController.swift
@@ -13,16 +13,28 @@ import Starscream
 class ChatViewController: UIViewController, WebSocketDelegate{
     var webSocket: WebSocket! = nil
     
+    @IBOutlet weak var labelInfo: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        webSocket = WebSocket(url: URL(string: "wss://conf.space/WebRTCHandsOnSig/jp_co_swift_devj")!)
+        webSocket.delegate = self
+        webSocket.connect()
+    }
+    
     @IBOutlet weak var remoteVideoView: RTCEAGLVideoView!
     @IBOutlet weak var cameraPreview: RTCCameraPreviewView!
     
     @IBAction func close(_ sender: Any) {
         LOG("")
+        webSocket.disconnect()
         dismiss(animated: true, completion: nil)
     }
     
     func websocketDidConnect(socket: WebSocket){
         LOG("")
+        labelInfo.text = "接続できたよ"
     }
     func websocketDidDisconnect(socket: WebSocket, error: NSError?){
         LOG("print:\(String(describing: error?.localizedDescription))")
